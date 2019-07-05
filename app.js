d9f71fdcd834e6d2c735138e91e71677c99ab359
JaquelineGDS <jaqueline_gd@outlook.com>
2019-07-05 17:36:42 -0300

    app

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require ("express"); //servidor
+const express = require ("express");
 const app = express();
 app.use(express.json());
 
 const db = require('./models/index');
 
-app.listen(4567, console.log("servidor rodando")); //porta
+app.listen(4567, console.log("servidor rodando"));
 
-app.use("/users", require('./routes/user')) //rota usuarios
-app.use("/products", require('./routes/products')) //rota produtos
+app.get('/', (req, resp) => {
+  resp.send( res.json(
+    {
+      "name": "bq-node",
+      "version": "1.0.0"
+    }
+  ));
+});
+app.use("/users", require('./routes/user'));
+app.use("/products", require('./routes/products'));
+app.use("/orders", require('./routes/orders'));
 
-// app.use("/orders", require('./routes/orders')) //rota orders
 
-db.sequelize.sync(); //sincronização com o banco
-
+db.sequelize.sync();
